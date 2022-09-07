7423511399675f5b640f0e0d9b7b2d90beb9de8b
conto336 <carlos.conto.336@gmail.com>
2022-09-07 12:52:03 -0600

    add thymeleaf

---
+++ src/main/java/firstweb/demo/config/WebConfig.java
@@ -0,0 +1,26 @@
+package firstweb.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Description;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Bean
+    @Description("Thymeleaf resolver serving HTML 5")
+    public ClassLoaderTemplateResolver templateResolver(){
+        var templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setPrefix("static/");
+        templateResolver.setCacheable(false);
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode("HTML5");
+        templateResolver.setCharacterEncoding("UTF-8");
+
+        return templateResolver;
+    }
+}
